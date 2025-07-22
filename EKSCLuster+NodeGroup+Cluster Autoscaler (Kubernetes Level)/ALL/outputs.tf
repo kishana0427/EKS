da82323baf9c2ce943d1ed41d02cac09940f23ca
kishana0427 <95524882+kishana0427@users.noreply.github.com>
2025-07-22 07:11:35 +0530

    Delete EKSCLuster+NodeGroup+Cluster Autoscaler (Kubernetes Level) directory

--- EKSCLuster+NodeGroup+Cluster Autoscaler (Kubernetes Level)/ALL/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "eks_cluster_name" {
-  value = aws_eks_cluster.eks_cluster.name
-}
-
-output "vpc_id" {
-  value = aws_vpc.eks_vpc.id
-}
--- EKSCLuster+NodeGroup+Cluster Autoscaler (Kubernetes Level)/ALL/provider.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-region = "ap-south-1"
-}
-
-provider "helm" {
-  kubernetes {
-    config_path = "~/.kube/config" # or use a data source if provisioning kubeconfig
-  }
-}
